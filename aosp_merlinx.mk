d8130b3eb00242546fc7eda821157ab0061fd53a
Archie <esanovalr.k@gmail.com>
2024-05-17 16:59:16 +0700

    Update and rename lineage_merlinx.mk to aosp_merlinx.mk

--- aosp_merlinx.mk
+++ aosp_merlinx.mk
@@ -11,10 +11,13 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from device makefile.
 $(call inherit-product, device/xiaomi/merlinx/device.mk)
 
-# Inherit some common LineageOS stuff.
+# Inherit some PixelBuilds stuff.
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+TARGET_BOOT_ANIMATION_RES := 1080
+TARGET_GAPPS_ARCH := arm64
+TARGET_FACE_UNLOCK_SUPPORTED := true
 
-PRODUCT_NAME := cherish_merlinx
+PRODUCT_NAME := aosp_merlinx
 PRODUCT_DEVICE := merlinx
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_BRAND := Redmi
